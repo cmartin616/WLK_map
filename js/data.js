d7248b68f844df495bac51f76858b4710adadf3b
Chris Martin <cmartin616@gmail.com>
2015-11-24 08:41:29 -0500

    added logan airport

--- js/data.js
+++ js/data.js
@@ -4,19 +4,21 @@ define({
     /* Home coords */
     wolftrapCoords: [38.910847, -77.248518],
     scotiaCoords: [42.829591, -73.979668],
+    danversCoords: [42.566539, -70.969733],
 
     /* Work coords */
     spcCoords: [40.439243, -79.991755],
     wyomissingCoords: [40.354292, -75.986396],
     esriRedlandsCoords: [34.056295, -117.195777],
     ogallalaCoords: [41.128175, -101.719501],
+    mediaCoords: [39.916747, -75.388089],
 
     /* Airport coords */
     albanyAirportCoords: [42.748334, -73.806700],
     charlotteAirportCoords: [35.216366, -80.951091],
     denverAirportCoords: [39.859786, -104.675798],
     dullesAirportCoords: [38.954985, -77.458942],
-    
+    loganAirportCoords: [42.366392, -71.016385],
     ontarioAirportCoords: [34.056405, -117.599602],
     phoenixAirportCoords: [33.437125, -112.007273],
     philadelphiaAirportCoords: [39.877953, -75.229548],
